d515ebb80764852b9bfe845efbc0766a32aafa0a
soulmate1139 <92954297+soulmate1139@users.noreply.github.com>
2024-11-01 10:55:17 +0100

    Add files via upload

---
+++ omni_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/788-87ebfa51b09f6bf7.js","static/css/0f15059519167bf3.css","static/chunks/pages/index-aca6be6fa06640d3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
